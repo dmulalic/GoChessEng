edd864f5d7e5100ce6af33110cc0519b37b67cf1
Denis Mulalic <mulalic83@gmail.com>
2022-08-07 12:38:56 -0700

    Added initSquare120to64

--- init.go
+++ init.go
@@ -12,6 +12,31 @@ func FileRank2Square(file int, rank int) int {
 	return ((21 + file) + (rank * 10))
 }
 
+func initSquare120to64() {
+	var index int
+	var file = FileA
+	var rank = Rank1
+	var square int = A1
+	var square64 int
+
+	for index = 0; index < BoardSquareNumber; index++ {
+		Square120To64[index] = 65
+	}
+
+	for index = 0; index < 64; index++ {
+		Square64To120[index] = 120
+	}
+
+	for rank = Rank1; rank <= Rank8; rank++ {
+		for file = FileA; file <= FileH; file++ {
+			square = FileRank2Square(file, rank)
+			Square64To120[square64] = square
+			Square120To64[square] = square64
+			square64++
+		}
+	}
+}
+
 func allInit() {
 
 	fmt.Println("allInit()")
