a731013c2d56b0258072f5efc9490c10fbe1f0f5
Denis Mulalic <mulalic83@gmail.com>
2022-08-07 14:11:53 -0700

    Add output.go

--- main.go
+++ main.go
@@ -2,28 +2,6 @@ package main
 
 import "fmt"
 
-func test() {
-	fmt.Println("---   DEBUG   ---")
-	for index := 0; index < BoardSquareNumber; index++ {
-		if index%10 == 0 {
-			fmt.Printf("\n")
-		}
-		fmt.Printf("%5d", Square120To64[index])
-	}
-	fmt.Println()
-	fmt.Println()
-
-	for index := 0; index < 64; index++ {
-		if index%8 == 0 {
-			fmt.Printf("\n")
-		}
-		fmt.Printf("%5d", Square64To120[index])
-	}
-
-	fmt.Printf("%v\n", NoSquare)
-	fmt.Println("--- END DEBUG ---")
-}
-
 func main() {
 	fmt.Printf("%v version %v\n", Name, Version)
 	fmt.Println("MIT license")
--- run.bash
+++ run.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-go run init.go defs.go main.go
+go run output.go init.go defs.go main.go
