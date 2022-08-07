c5309ac7172ccb4c0ea968b1c793b6c3c91f4a71
Denis Mulalic <demu4731@colorado.edu>
2022-08-06 18:38:59 -0700

    Add board squares

--- src/main.go
+++ src/main.go
@@ -49,6 +49,72 @@ const (
 	RankNone
 )
 
+const (
+	A1 int = iota + 21
+	B1
+	C1
+	D1
+	E1
+	F1
+	G1
+	H1
+	A2 int = iota + 31
+	B2
+	C2
+	D2
+	E2
+	F2
+	G2
+	H2
+	A3 int = iota + 41
+	B3
+	C3
+	D3
+	E3
+	F3
+	G3
+	H3
+	A4 int = iota + 51
+	B4
+	C4
+	D4
+	E4
+	F4
+	G4
+	H4
+	A5 int = iota + 61
+	B5
+	C5
+	D5
+	E5
+	F5
+	G5
+	H5
+	A6 int = iota + 71
+	B6
+	C6
+	D6
+	E6
+	F6
+	G6
+	H6
+	A7 int = iota + 81
+	B7
+	C7
+	D7
+	E7
+	F7
+	G7
+	H7
+	A8 int = iota + 91
+	B8
+	C8
+	D8
+	E8
+	F8
+	G8
+	H8
+)
 func main() {
 	fmt.Printf("%v version %v\n", Name, Version)
 	fmt.Println("MIT license")
